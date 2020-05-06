c1920604ccd39980bd0fe833689b2babe0127828
pxckets <49133592+pxckets@users.noreply.github.com>
2020-05-05 23:00:17 -0400

    more

--- main.py
+++ main.py
@@ -7,26 +7,23 @@ from random import randint
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix='>>') 
+bot.remove_command("help")
 
 tracks = ['']
 
-
-####################################################################################
 @bot.command(pass_context=True) 
 @commands.cooldown(1, 3600, commands.BucketType.user)
 async def submit(ctx, arg):
     channel = bot.get_channel(707329253422661682)
     track = arg
+    track_id = len(tracks)
     if "https://soundcloud.com" in track:
 
-        tracks.append(arg)
-        await ctx.send('Submitted your track!')
+        tracks.append(str(track_id) + "|" + arg)
+        await ctx.send('Submitted your link!')
     else:
         await channel.send("Invalid URL, must be soundcloud")
-######################################################################################
 
-
-#######################################################################################
 @bot.command()
 @commands.has_permissions(ban_members=True)
 async def lottery(ctx):
@@ -34,32 +31,48 @@ async def lottery(ctx):
     selection = randint(1, len(tracks) - 1)
     await channel.send('@everyone THE WINNER IS ' + tracks[selection])
     del tracks[:]
-#######################################################################################
 
-
-######################################################################################
 @bot.command()
 @commands.has_permissions(ban_members=True)
 async def clearlist(ctx):
     del tracks[:]
-#######################################################################################
 
-
-########################################################################################
 @bot.command()
-async def tracklist(ctx):
+async def linklist(ctx):
     channel = bot.get_channel(707329253422661682)
     await channel.send('```' + '\n'.join(map(str, tracks)) + '```') 
-########################################################################################  
+
+@bot.command(pass_context=True)
+@commands.has_permissions(ban_members=True)
+async def removetrack(ctx, arg):
+    channel = bot.get_channel(707329253422661682)
+    tracks.pop(int(arg))
+    await channel.send("Track " + str(arg) + " removed.")
 
 @bot.command()
 async def insxne(ctx):
     channel = bot.get_channel(707329253422661682)
     await channel.send("https://soundcloud.com/insxnebeats")
 
+@bot.command()
+async def ping(ctx):
+    await ctx.send('> Pong! {0}'.format(round(bot.latency, 1)))
+
+@bot.command(pass_context=True)
+async def help(ctx):
+    channel = bot.get_channel(707329253422661682)
+    await channel.send("""
+    > submit      |    for submitting a track or soundcloud prfile
+    > linklist       |   for viewing all links in the lottery
+    > ping           |   get the bots latency
+    > insxne       |   developers soundcloud <3
+    """)
+
+
 @bot.event 
 async def on_ready():
     print('Logged in as:')
     print(bot.user.name)
+    await bot.change_presence(activity=discord.Game(name="Prefix: >>"))
 
 bot.run('')
